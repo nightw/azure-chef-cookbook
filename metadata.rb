cc79cbff3fa20b17ed4f8e90c32dcc529393b2e2
Pal David Gergely <nightw17@gmail.com>
2014-06-12 23:20:30 +0200

    Berkshelf compatible version number

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "nightw17@gmail.com"
 license          "Apache 2.0"
 description      "Installs azure-cli npm package"
 
-version          "0.1"
+version          "0.1.0"
 depends          "ark"
